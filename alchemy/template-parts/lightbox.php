73c3cd1ec0b385e4a304a8755a6cf08b9cc67f9b
benjamin <benjbruant@gmail.com>
2024-11-14 12:59:38 +0100

    lightbox css arrows

--- alchemy/template-parts/lightbox.php
+++ alchemy/template-parts/lightbox.php
@@ -1,19 +1,18 @@
-
-<div id="lightbox" data-src="test" data-id="" data-reference="<?php echo get_field("reference")?>" data-categorie="">
-    <div class="close-button">&#10539;EtCaetera</div>
+<div id="lightbox" data-src="test" data-id="" data-reference="<?php echo get_field("reference") ?>" data-categorie="">
+    <div class="close-button">&#10539;</div>
     <a href="">
-            <img class="nav-lightbox" id="prev-img" src="<?php echo get_template_directory_uri() . "/assets/icones/prev-img.svg"?>" alt="">
-        </a>
-            <div class="img-container">
-                <img src="" width="800" alt="">
-                <div class="data">
-                <span class="reference-img"></span>
-                <span class="categorie-img"></span>
-                </div>
-            </div>
-        <a href="">
-            <img class="nav-lightbox" id="next-img" src="<?php echo get_template_directory_uri() . "/assets/icones/next-img.svg"?>" alt="">
-        </a>
+        <img class="nav-lightbox" id="prev-img" src="<?php echo get_template_directory_uri() . "/assets/icones/prev-img.svg" ?>" alt="">
+    </a>
+    <div class="img-container">
+        <img src="" width="800" alt="">
+        <div class="data">
+            <span class="reference-img"></span>
+            <span class="categorie-img"></span>
+        </div>
+    </div>
+    <a href="">
+        <img class="nav-lightbox" id="next-img" src="<?php echo get_template_directory_uri() . "/assets/icones/next-img.svg" ?>" alt="">
+    </a>
 </div>
 
 <?php
